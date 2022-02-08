446bab966addd83906d14001aa6b8c3257a07712
Shane Slone <Shane.X.Slone@kp.org>
2022-02-08 15:16:10 -0500

    fixed error from merge

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import TruckMap from "./components/TruckMap";
-import Login from "./components/Login";
-import UserForm from "./components/UserForm";
-import PrivateRoute from "./components/PrivateRoute";
-import AddTruck from "./components/AddTruck";
-import AddMenuItem from "./components/AddMenuItem";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import TruckMap from './components/TruckMap';
+import Login from './components/Login';
+import AddUserForm from './components/AddUserForm';
+import PrivateRoute from './components/PrivateRoute';
+import AddTruck from './components/AddTruck';
+import AddMenuItem from './components/AddMenuItem';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/map" element={<PrivateRoute component={TruckMap} />} />
+        <Route path='/map' element={<PrivateRoute component={TruckMap} />} />
         <Route
-          path="/addtruck"
+          path='/addtruck'
           element={<PrivateRoute component={AddTruck} />}
         />
         <Route
-          path="/truck/:truckId/addmenuitem"
+          path='/truck/:truckId/addmenuitem'
           element={<PrivateRoute component={AddMenuItem} />}
         />
-        <Route path="signup" element={<UserForm />} />
-        <Route path="/" element={<Login />} />
+        <Route path='signup' element={<AddUserForm />} />
+        <Route path='/' element={<Login />} />
       </Routes>
     </Router>
   );
