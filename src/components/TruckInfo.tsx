4e90bce1dc74d684afb3a10907e4f1719c5f112f
AftonSlone <afton.slone@gmail.com>
2022-02-15 00:05:08 -0500

    Updates to Truck Info

--- src/components/TruckInfo.tsx
+++ src/components/TruckInfo.tsx
@@ -1,17 +1,28 @@
-import { Col, Container, Image, ListGroup, Row } from "react-bootstrap";
+import { Button, Col, Container, Image, ListGroup, Row } from "react-bootstrap";
 import useFetchTruck from "../hooks/useFetchTruck";
 import LoadingSpinner from "./LoadingSpinner";
+import { Rating } from "react-simple-star-rating";
 import MenuItemCard from "./MenuItemCard";
 
 const TruckInfo = () => {
-  const [currentTruck, Loading, errorMessage] = useFetchTruck();
+  const [
+    currentTruck,
+    Loading,
+    errorMessage,
+    rating,
+    handleRating,
+    getCustomerRating,
+  ] = useFetchTruck();
 
   if (Loading) return <LoadingSpinner />;
   return (
     <Container className="d-flex align-items-center flex-column bg-danger">
-      <Row className="w-auto">
+      <Row>
         <Col className="d-flex justify-content-center m-2">
           {currentTruck.name}
+        </Col>
+        <Col className="d-flex justify-content-center m-2">
+          <Button>Add to Favorites</Button>
         </Col>
       </Row>
       <Row className="w-75 m-2">
@@ -29,8 +40,39 @@ const TruckInfo = () => {
           <ListGroup>
             <ListGroup.Item className="d-flex justify-content-center">{`Cuisine Type: ${currentTruck.cuisineType}`}</ListGroup.Item>
             <ListGroup.Item className="d-flex justify-content-center">{`Departure Time: ${currentTruck.departureTime}`}</ListGroup.Item>
-            <ListGroup.Item className="d-flex justify-content-center">
-              {`Avarage Customer Review: ${currentTruck.customerRatingsAvg}`}
+            <ListGroup.Item className="d-flex justify-content-center align-items-center">
+              Average Customer Rating:{" "}
+              <Rating
+                ratingValue={0}
+                initialValue={currentTruck.customerRatingsAvg}
+                allowHalfIcon
+                transition
+                readonly
+                fillColorArray={[
+                  "#f17a45",
+                  "#f19745",
+                  "#f1a545",
+                  "#f1b345",
+                  "#f1d045",
+                ]}
+              />
+            </ListGroup.Item>
+            <ListGroup.Item className="d-flex justify-content-center align-items-center">
+              Your Rating:{" "}
+              <Rating
+                ratingValue={rating}
+                initialValue={getCustomerRating()}
+                onClick={handleRating}
+                allowHalfIcon
+                transition
+                fillColorArray={[
+                  "#f17a45",
+                  "#f19745",
+                  "#f1a545",
+                  "#f1b345",
+                  "#f1d045",
+                ]}
+              />
             </ListGroup.Item>
           </ListGroup>
         </Col>
--- src/hooks/useFetchTruck.ts
+++ src/hooks/useFetchTruck.ts
@@ -1,10 +1,18 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { fetchCurrentTruck } from "../store/actions/trucks";
-import { RootState, Truck } from "../types";
+import { RootState, Truck, User } from "../types";
+import axiosWithAuth from "../utils/axoisWithAuth";
 
-export default function useFetchTruck(): [Truck, Boolean, String] {
+export default function useFetchTruck(): [
+  Truck,
+  Boolean,
+  String,
+  number,
+  (rating: number) => Promise<void>,
+  () => number
+] {
   const dispatch = useDispatch();
   const { truckId } = useParams();
 
@@ -20,9 +28,35 @@ export default function useFetchTruck(): [Truck, Boolean, String] {
     (state) => state.trucks.errorMessage
   );
 
+  const user = useSelector<RootState, User>((state) => state.user.user);
+
+  const [rating, setRating] = useState<number>(0);
+
+  const handleRating = async (rating: number) => {
+    setRating(rating);
+    await axiosWithAuth().post(
+      `trucks/truck/${currentTruck.truckId}/rating/${rating}`
+    );
+  };
+
+  const getCustomerRating = () => {
+    const result = currentTruck.reviews.filter(
+      (review) => review.diner.userid === user.userid
+    );
+    if (result.length) return result[0].score;
+    return 0;
+  };
+
   useEffect(() => {
     if (truckId) dispatch(fetchCurrentTruck(truckId));
   }, [dispatch, truckId]);
 
-  return [currentTruck, loading, errorMessage];
+  return [
+    currentTruck,
+    loading,
+    errorMessage,
+    rating,
+    handleRating,
+    getCustomerRating,
+  ];
 }
