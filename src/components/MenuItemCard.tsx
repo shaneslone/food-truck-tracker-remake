6df14b3da5c2a9d9b316513a0d447dae47a740fb
Shane Slone <Shane.X.Slone@kp.org>
2022-02-17 15:43:16 -0500

    changed size of star rating

--- src/components/MenuItemCard.tsx
+++ src/components/MenuItemCard.tsx
@@ -1,7 +1,7 @@
-import { Card, Carousel, Col, Container, Image, Row } from "react-bootstrap";
-import { MenuItem } from "../types";
-import { Rating } from "react-simple-star-rating";
-import useMenuCardItem from "../hooks/useMenuCardItem";
+import { Card, Carousel, Col, Container, Image, Row } from 'react-bootstrap';
+import { MenuItem } from '../types';
+import { Rating } from 'react-simple-star-rating';
+import useMenuCardItem from '../hooks/useMenuCardItem';
 
 interface Iprops {
   menuItem: MenuItem;
@@ -12,17 +12,17 @@ const MenuItemCard: React.FC<Iprops> = ({ menuItem }) => {
     useMenuCardItem(menuItem);
 
   return (
-    <Card className="w-100">
-      <Card.Header className="d-flex justify-content-center">
+    <Card className='w-100'>
+      <Card.Header className='d-flex justify-content-center'>
         {menuItem.itemName}
       </Card.Header>
-      <Card.Body className="d-flex align-items-center flex-column">
+      <Card.Body className='d-flex align-items-center flex-column'>
         {menuItem.itemPhotos.length === 1 && (
-          <Image src={menuItem.itemPhotos[0].url} style={{ width: "100%" }} />
+          <Image src={menuItem.itemPhotos[0].url} style={{ width: '100%' }} />
         )}
         {menuItem.itemPhotos.length > 1 && (
           <Carousel>
-            {menuItem.itemPhotos.map((photo) => {
+            {menuItem.itemPhotos.map(photo => {
               return (
                 <Carousel.Item>
                   <Image src={photo.url} />
@@ -31,54 +31,56 @@ const MenuItemCard: React.FC<Iprops> = ({ menuItem }) => {
             })}
           </Carousel>
         )}
-        <Card.Text className="d-flex justify-content-center">
+        <Card.Text className='d-flex justify-content-center'>
           {menuItem.itemDescription}
         </Card.Text>
-        <Card.Text className="d-flex justify-content-center">
+        <Card.Text className='d-flex justify-content-center'>
           {`Price: ${menuItem.itemPrice}`}
         </Card.Text>
-        <Container className="d-flex align-items-center flex-column">
-          <Row className="w-auto">
+        <Container className='d-flex align-items-center flex-column'>
+          <Row className='w-auto'>
             <Col>Customer Average Rating</Col>
           </Row>
-          <Row className="w-auto">
+          <Row className='w-auto'>
             <Col>
               <Rating
                 ratingValue={0}
+                size={20}
                 initialValue={menuItem.customerRatingsAvg}
                 allowHalfIcon
                 transition
                 readonly
                 fillColorArray={[
-                  "#f17a45",
-                  "#f19745",
-                  "#f1a545",
-                  "#f1b345",
-                  "#f1d045",
+                  '#f17a45',
+                  '#f19745',
+                  '#f1a545',
+                  '#f1b345',
+                  '#f1d045',
                 ]}
               />
             </Col>
           </Row>
           {isDiner && (
-            <Row className="w-auto">
+            <Row className='w-auto'>
               <Col>Your Rating</Col>
             </Row>
           )}
           {isDiner && (
-            <Row className="w-auto">
+            <Row className='w-auto'>
               <Col>
                 <Rating
                   ratingValue={rating}
+                  size={20}
                   initialValue={getCustomerRating()}
                   onClick={handleRating}
                   allowHalfIcon
                   transition
                   fillColorArray={[
-                    "#f17a45",
-                    "#f19745",
-                    "#f1a545",
-                    "#f1b345",
-                    "#f1d045",
+                    '#f17a45',
+                    '#f19745',
+                    '#f1a545',
+                    '#f1b345',
+                    '#f1d045',
                   ]}
                 />
               </Col>
--- src/components/TruckInfo.tsx
+++ src/components/TruckInfo.tsx
@@ -1,11 +1,10 @@
-import { Button, Col, Container, Image, ListGroup, Row } from "react-bootstrap";
-import useFetchTruck from "../hooks/useFetchTruck";
-import LoadingSpinner from "./LoadingSpinner";
-import { Rating } from "react-simple-star-rating";
-import MenuItemCard from "./MenuItemCard";
+import { Button, Col, Container, Image, ListGroup, Row } from 'react-bootstrap';
+import useFetchTruck from '../hooks/useFetchTruck';
+import LoadingSpinner from './LoadingSpinner';
+import { Rating } from 'react-simple-star-rating';
+import MenuItemCard from './MenuItemCard';
 
-const TruckInfo = () =>{
-
+const TruckInfo = () => {
   const [
     currentTruck,
     isDiner,
@@ -20,71 +19,79 @@ const TruckInfo = () =>{
 
   if (Loading) return <LoadingSpinner />;
   return (
-    <Container className="d-flex align-items-center flex-column bg-danger">
-      <Row className="d-flex justify-content-center align-items-center">
-        <Col className="d-flex justify-content-center m-2">
+    <Container className='d-flex align-items-center flex-column'>
+      <Row className='d-flex justify-content-center align-items-center'>
+        <Col className='d-flex justify-content-center m-2'>
           {currentTruck.name}
         </Col>
-        <Col className="d-flex justify-content-center m-2">
-          {isDiner && <Button onClick={handleFavoriteTruck}>
-            {getFavoriteTruck() ? "Remove from Favorites" : "Add to Favorites"}
-          </Button>}
+        <Col className='d-flex justify-content-center m-2'>
+          {isDiner && (
+            <Button onClick={handleFavoriteTruck}>
+              {getFavoriteTruck()
+                ? 'Remove from Favorites'
+                : 'Add to Favorites'}
+            </Button>
+          )}
         </Col>
       </Row>
-      <Row className="w-75 m-2">
+      <Row className='w-75 m-2'>
         <Col>
           <Image
             style={{
-              width: "100%",
+              width: '100%',
             }}
             src={currentTruck.imageOfTruck}
           />
         </Col>
       </Row>
-      <Row className="w-75 m-2">
+      <Row className='w-75 m-2'>
         <Col>
           <ListGroup>
-            <ListGroup.Item className="d-flex justify-content-center">{`Cuisine Type: ${currentTruck.cuisineType}`}</ListGroup.Item>
-            <ListGroup.Item className="d-flex justify-content-center">{`Departure Time: ${currentTruck.departureTime}`}</ListGroup.Item>
-            <ListGroup.Item className="d-flex justify-content-center align-items-center">
-              Average Customer Rating:{" "}
+            <ListGroup.Item className='d-flex justify-content-center'>{`Cuisine Type: ${currentTruck.cuisineType}`}</ListGroup.Item>
+            <ListGroup.Item className='d-flex justify-content-center'>{`Departure Time: ${currentTruck.departureTime}`}</ListGroup.Item>
+            <ListGroup.Item className='d-flex justify-content-center align-items-center'>
+              Average Customer Rating:{' '}
               <Rating
                 ratingValue={0}
                 initialValue={currentTruck.customerRatingsAvg}
                 allowHalfIcon
                 transition
                 readonly
+                size={20}
                 fillColorArray={[
-                  "#f17a45",
-                  "#f19745",
-                  "#f1a545",
-                  "#f1b345",
-                  "#f1d045",
+                  '#f17a45',
+                  '#f19745',
+                  '#f1a545',
+                  '#f1b345',
+                  '#f1d045',
                 ]}
               />
             </ListGroup.Item>
-            {isDiner && <ListGroup.Item className="d-flex justify-content-center align-items-center">
-              Your Rating:{" "}
-              <Rating
-                ratingValue={rating}
-                initialValue={getCustomerRating()}
-                onClick={handleRating}
-                allowHalfIcon
-                transition
-                fillColorArray={[
-                  "#f17a45",
-                  "#f19745",
-                  "#f1a545",
-                  "#f1b345",
-                  "#f1d045",
-                ]}
-              />
-            </ListGroup.Item>}
+            {isDiner && (
+              <ListGroup.Item className='d-flex justify-content-center align-items-center'>
+                Your Rating:{' '}
+                <Rating
+                  ratingValue={rating}
+                  initialValue={getCustomerRating()}
+                  onClick={handleRating}
+                  allowHalfIcon
+                  transition
+                  size={20}
+                  fillColorArray={[
+                    '#f17a45',
+                    '#f19745',
+                    '#f1a545',
+                    '#f1b345',
+                    '#f1d045',
+                  ]}
+                />
+              </ListGroup.Item>
+            )}
           </ListGroup>
         </Col>
       </Row>
-      <Row xs={1} md={2} className="w-75 m-2">
-        {currentTruck.menu.map((menuItem) => (
+      <Row xs={1} md={2} className='w-75 m-2'>
+        {currentTruck.menu.map(menuItem => (
           <Col key={menuItem.menuId}>
             <MenuItemCard menuItem={menuItem} />
           </Col>
