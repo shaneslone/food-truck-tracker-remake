f79b4e6e22b95522d83a959f511b405ad4a7ecda
AftonSlone <afton.slone@gmail.com>
2022-03-07 17:32:31 -0500

    Working on add user form tests

---
+++ src/__Tests__/AddUserForm.test.tsx
@@ -0,0 +1,10 @@
+import {
+  Matcher,
+  MatcherOptions,
+  render,
+  waitForOptions,
+} from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import "@testing-library/jest-dom";
+
+describe("AddUserForm", () => {});
